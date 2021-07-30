d7cf70ed1996e441ef242fc10874590004a759d4
Dmytro Melnyk <melnykecho@gmail.com>
2021-07-30 21:41:54 +0300

    1 commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-import "./css/styles.css";
 
-console.log("Мастерская: деплой билда от Parcel на GitHub Pages");
+import foodCardTpl from './templates/food-card.hbs';
+import foodCards from './menu.json';
+
+const refs = {
+  switchMarker: document.querySelector('.theme-switch__marker'),
+  themeCheckbox: document.querySelector('#theme-switch-toggle'),
+  menu: document.querySelector('.menu'),
+  body: document.querySelector('body'),
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+//разметкa
+const menuMarkup = foodCards.map(card => foodCardTpl(card)).join('');
+refs.menu.innerHTML = menuMarkup;
+
+// установка темы
+populateTheme();
+
+function populateTheme() {
+  const localStorageTheme = localStorage.getItem('theme');
+  if (localStorageTheme === Theme.DARK) {
+    refs.body.className = Theme.DARK;
+    refs.themeCheckbox.checked = true;
+    refs.switchMarker.style.transition = 'none';
+  } else {
+    refs.body.className = Theme.LIGHT;
+  }
+}
+
+// изменение темы
+refs.themeCheckbox.addEventListener('change', onChangeTheme);
+
+function onChangeTheme() {
+  refs.switchMarker.style.transition = '';
+  if (refs.body.className === Theme.DARK) {
+    refs.body.className = Theme.LIGHT;
+    localStorage.setItem('theme', Theme.LIGHT);
+  } else {
+    refs.body.className = Theme.DARK;
+    localStorage.setItem('theme', Theme.DARK);
+  }
+}
